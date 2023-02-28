5fd181a46439cc5e7f87cbada29b86041c4f7776
knikhilesh <nikhileshk.dexbytes@gmail.com>
2023-02-28 19:58:00 +0530

    small changes

--- lib/src/pages/destination_filter_screen.dart
+++ lib/src/pages/destination_filter_screen.dart
@@ -2,6 +2,8 @@ import 'package:base_flutter_app/src/all_file_import/app_values_files_link.dart'
 import 'package:base_flutter_app/src/all_file_import/app_widget_files_link.dart';
 import 'package:base_flutter_app/src/image_res/iconApp.dart';
 import 'package:flutter/material.dart';
+import 'package:snippet_coder_utils/FormHelper.dart';
+import 'package:syncfusion_flutter_sliders/sliders.dart';
 
 import '../widgets/appbar/home_screen_app_bar.dart';
 
@@ -26,6 +28,42 @@ class _DestinationFilterScreenState extends State<DestinationFilterScreen> {
     'search': "",
   };
 
+  final List <String> searchResultViewOptionList = [
+    "Vertical Card",
+    "Horizontal Card"
+  ];
+
+  List<dynamic> countryNameList = [];
+  List<dynamic> cityNameList = [];
+  List<dynamic> city = [];
+
+  String? countryId;
+  String? cityId;
+
+  int selectedIndex = 0;
+  String countryValue = "";
+  String stateValue = "";
+  String cityValue = "";
+  String address = "";
+  SfRangeValues _values = SfRangeValues(40.0, 80.0);
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    this.countryNameList.add({"id":1,"name" : "India", });
+    this.countryNameList.add({"id":2,"name" : "UAE", });
+
+    this.cityNameList = [
+      {"ID": 1, "Name" : "Delhi", "ParentId" : 1},
+      {"ID": 2, "Name" : "Indore", "ParentId" : 1},
+      {"ID": 3, "Name" : "Mumbai", "ParentId" : 1},
+      {"ID": 4, "Name" : "Bhopal", "ParentId" : 1},
+      {"ID": 5, "Name" : "Gwalior", "ParentId" : 1},
+      {"ID": 1, "Name" : "Abu Dhabi", "ParentId" : 2},
+      {"ID": 2, "Name" : "Dubai", "ParentId" : 2},
+    ];
+  }
   
   @override
   Widget build(BuildContext context) {
@@ -33,15 +71,10 @@ class _DestinationFilterScreenState extends State<DestinationFilterScreen> {
     AppDimens appDimens = AppDimens();
     appDimens.appDimensFind(context: context);
 
-    final List <String> searchResultViewOptionList = [
-      "Vertical Card",
-      "Horizontal Card"
-    ];
-
     // Filter search result view option card
     Widget filterSearchResultViewOption(){
       return Container(
-        height: 40,
+        height: 38,
         child: ListView.builder(
             itemCount: searchResultViewOptionList.length,
             scrollDirection: Axis.horizontal,
@@ -49,28 +82,116 @@ class _DestinationFilterScreenState extends State<DestinationFilterScreen> {
             shrinkWrap: true,
             physics: ClampingScrollPhysics(),
             itemBuilder: (context,index){
-              return Container(
-                padding: EdgeInsets.all(14),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  color: appColors.appBgColor2.withOpacity(0.30)
-                ),
-                child: Text(searchResultViewOptionList[index],
-                style: appStyles.commonSubTitleTextStyle(fontSize: 13),
+              return GestureDetector(
+                onTap: (){
+                  setState(() {
+                    this.selectedIndex = index;
+                  });
+                },
+                child: Container(
+                  padding: EdgeInsets.all(10).copyWith(left: 12,right: 12),
+                  margin: EdgeInsets.only(right: 10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: selectedIndex == index ? appColors.appBgColor1.withOpacity(0.15) : appColors.appTransColor,
+                    border: Border.all(color:  selectedIndex == index
+                        ? appColors.appTransColor
+                        : appColors.appBgColor1.withOpacity(0.15),
+                        width: selectedIndex == index
+                        ? 0
+                        :1.5)
+                  ),
+                  child: Text(searchResultViewOptionList[index],
+                  style: appStyles.commonSubTitleTextStyle(
+                      fontSize: 11.5, texColor: selectedIndex == index
+                      ? appColors.buttonBgColor
+                      :appColors.textColor
+                  ),
+                  ),
                 ),
               );
             }
         ),
       );
     }
+
+    // Location drop down
+    Widget locationDropDownView(){
+      return Container(
+        padding: EdgeInsets.only(right: 80),
+        height: 100,
+        child: Column(
+            children: [
+              FormHelper.dropDownWidget(
+                  context,
+                  "Country",
+                  this.countryId,
+                  this.countryNameList,
+                  (onChangedVal){
+                    this.countryId = onChangedVal;
+                    this.city = cityNameList.where(
+                            (cityItem) => cityItem["ParentId"].toString() == onChangedVal.toString()).toList();
+                    this.cityId = null;
+                    },
+                  (onValidateVal){
+                    if(onValidateVal == null){
+                      return "Please select country";
+                    }
+                    return null;
+                  },
+                borderColor: appColors.buttonBgColor,
+                borderFocusColor: appColors.appThemeColor,
+                borderRadius: 10,
+              ),
+              FormHelper.dropDownWidget(
+                  context,
+                  "City",
+                  this.cityId,
+                  this.cityNameList,
+                  (onChangedVal){
+                    this.cityId = onChangedVal;
+                  },
+                  (onValidateVal){
+                  return null;
+                  },
+                borderColor: appColors.buttonBgColor,
+                borderFocusColor: appColors.appThemeColor,
+                borderRadius: 10,
+                optionValue: "ID",
+                optionLabel: "Name"
+              )
+            ]
+        ),
+      );
+    }
+
+    // Price value slider
+    Widget priceValueSlider(){
+      return SfRangeSlider(
+        min: 0.0,
+        max: 100.0,
+        values: _values,
+        interval: 20,
+        showTicks: false,
+        showLabels: true,
+        enableTooltip: true,
+        minorTicksPerInterval: 1,
+        onChanged: (SfRangeValues values){
+          setState(() {
+            _values = values;
+          });
+        },
+      );
+    }
+
     // Filter pop up dialog
-    Future filterPopUpDialog() {
+    filterPopUpDialog() {
       return showDialog(
           context: context,
           barrierDismissible: true,
           builder: (context) {
             return SimpleDialog(
-              backgroundColor: appColors.appBgColor2,
+              backgroundColor: appColors.appBgColorJungleGreen,
               insetPadding: EdgeInsets.all(20),
               contentPadding: EdgeInsets.all(20),
               shape: RoundedRectangleBorder(
@@ -82,32 +203,50 @@ class _DestinationFilterScreenState extends State<DestinationFilterScreen> {
                     Container(
                       height: appDimens.heightFullScreen() - 425,
                       width: appDimens.widthFullScreen() - 20,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Text(appString.trans(context, appString.viewTitleText),
-                            style: appStyles.commonSubTitleTextStyle(fontSize: 15),
-                          ),
-                          // filterSearchResultViewOption(),
-                          Container(
-                            width: double.infinity,
-                            height: 60,
-                            child: ElevatedButton(
-                              onPressed: () {
+                      child: SingleChildScrollView(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Text(appString.trans(context, appString.viewTitleText),
+                              style: appStyles.commonSubTitleTextStyle(fontSize: 15),
+                            ),
+                            SizedBox(height: 10,),
+                            filterSearchResultViewOption(),
+                            SizedBox(height: 35,),
+                            Text(appString.trans(context, appString.locationTitleText),
+                              style: appStyles.commonSubTitleTextStyle(fontSize: 15),
+                            ),
+                            SizedBox(height: 10,),
+                            locationDropDownView(),
+                            SizedBox(height: 10,),
+                            priceValueSlider(),
+                            SizedBox(height: 35,),
+                            Text(appString.trans(context, appString.priceTitleText),
+                              style: appStyles.commonSubTitleTextStyle(fontSize: 15),
+                            ),
+                            SizedBox(height: 35,),
+                            Text(appString.trans(context, appString.ratingTitleText),
+                              style: appStyles.commonSubTitleTextStyle(fontSize: 15),
+                            ),
+                            SizedBox(height: 35,),
+                            Text(appString.trans(context, appString.rangeTitleText),
+                              style: appStyles.commonSubTitleTextStyle(fontSize: 15),
+                            ),
+                            SizedBox(height: 35,),
+                            CommonButton(
+                              buttonName: appString.trans(context, appString.applyFilterButtonText),
+                              buttonHeight: 50,
+                              buttonBorderRadius: 18,
+                              isBottomMarginRequired: false,
+                              textStyle: appStyles.buttonNameStyle(),
+                              backCallback: (){
                                 Navigator.of(context).pop();
                               },
-                              style: ElevatedButton.styleFrom(
-                                primary: Colors.black,
-                                // fixedSize: Size(250, 50),
-                              ),
-                              child: Text(
-                                "Submit",
-                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     )
               ],
@@ -188,15 +327,18 @@ class _DestinationFilterScreenState extends State<DestinationFilterScreen> {
               Row(
                 children: [
                   Text(appString.trans(context, appString.foundText),
-                    style: appStyles.commonSubTitleTextStyle(fontWeight: FontWeight.w600),
+                    style: appStyles.commonSubTitleTextStyle(fontWeight: FontWeight.w600,
+                    fontSize: 15),
                   ),
                   SizedBox(width: 3,),
                   Text("90+",
-                    style: appStyles.commonSubTitleTextStyle(fontWeight: FontWeight.w600),
+                    style: appStyles.commonSubTitleTextStyle(fontWeight: FontWeight.w600,
+                    fontSize: 15),
                   ),
                   SizedBox(width: 3,),
                   Text(appString.trans(context, appString.destinationText),
-                    style: appStyles.commonSubTitleTextStyle(fontWeight: FontWeight.w600),
+                    style: appStyles.commonSubTitleTextStyle(fontWeight: FontWeight.w600,
+                    fontSize: 15),
                   ),
                 ],
               ),
